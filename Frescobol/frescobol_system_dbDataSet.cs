6a84a5a0fc31234fde69ac393b710eda0a861fa9
Luiz Eduardo <eduardo@favalessa.com.br>
2016-12-19 20:10:09 -0200

    Criado formulário principal da aplicação.

---
+++ Frescobol/frescobol_system_dbDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Frescobol
+{
+
+
+    partial class frescobol_system_dbDataSet
+    {
+    }
+}
+
+namespace Frescobol.frescobol_system_dbDataSetTableAdapters {
+    
+    
+    public partial class atletaTableAdapter {
+    }
+}
