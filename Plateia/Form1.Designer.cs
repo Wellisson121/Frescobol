8b89f288a312f96f3aac8f0d9936f0102c8c3ae3
Luiz Eduardo <eduardo@favalessa.com.br>
2017-01-14 14:08:34 -0200

    inicio da tela de plateia

--- Plateia/Form1.Designer.cs
+++ Plateia/Form1.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cronômetro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 82F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(543, 126);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00.000";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 456);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Plateia/Form1.cs
+++ Plateia/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,45 @@ namespace Plateia
 {
     public partial class Form1 : Form
     {
+        private Stopwatch cronometro;
+        private Timer cronTimer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized; //inicializa maximizado
+
+            this.cronometro = new Stopwatch();
+            this.cronTimer = new Timer();
+            this.cronTimer.Interval = 10; //10 ms
+            this.cronTimer.Tick += updateCronometroText;
+
+        }
+
+        private void updateCronometroText(object sender, EventArgs e)
+        {
+            if(this.cronometro.IsRunning)
+            {
+                this.label2.Text = new DateTime(this.cronometro.Elapsed.Ticks).ToString("mm:ss.fff");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //start / stop cronometro
+            if(this.cronometro.IsRunning == true)
+            {
+                this.cronTimer.Stop();
+                this.cronometro.Stop();
+            } else
+            {
+                this.cronTimer.Start();
+                this.cronometro.Start();
+            }
         }
     }
 }
